4e5e1df3d2e8636e057ce17c897ce9ee6bc9d7bf
xueweidong <xueweidong@iboxpay.com>
2019-09-26 15:51:27 +0800

    新建cloud config

--- cloud-client/src/main/java/com/spring/cloudclient/CloudClientApplication.java
+++ cloud-client/src/main/java/com/spring/cloudclient/CloudClientApplication.java
@@ -3,11 +3,12 @@ package com.spring.cloudclient;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableEurekaClient
-@MapperScan("com.spring.cloudclient.mapper")
+//@MapperScan("com.spring.cloudclient.mapper")
 public class CloudClientApplication {
 
     public static void main(String[] args) {
--- consumer-feign/src/main/java/com/spring/consumerfeign/ConsumerFeignApplication.java
+++ consumer-feign/src/main/java/com/spring/consumerfeign/ConsumerFeignApplication.java
@@ -3,13 +3,14 @@ package com.spring.consumerfeign;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableDiscoveryClient
 @EnableFeignClients //开启feign的功能
-@MapperScan("com.spring.consumerfeign.mapper")
+//@MapperScan("com.spring.consumerfeign.mapper")
 public class ConsumerFeignApplication {
 
     public static void main(String[] args) {
